04387ff3f75a4de506115f1e408388050a172708
AroobaRehman <82054640+AroobaRehman@users.noreply.github.com>
2026-07-01 19:35:36 +0000

    added crud.py

---
+++ chapter3/crud.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import joinedload
+from datetime import date 
+import models
+
+def get_player(db:Session, player_id:int):
+    return db.query(models.Player).filter(models.Player.player_id == player_id).first()
+
